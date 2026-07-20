b13cea285cb4eca116c244b6d6620d67d71ca641
Laiqa Uroon <sp24-bai-019@cuilahore.edu.pk>
2026-07-20 13:43:03 +0500

    Complete CS50P Problem Set 6 - pizza

---
+++ PROBLEM_SET_6/pizza.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import tabulate
+import csv
+def main() :
+    if len(sys.argv) > 2 :
+        sys.exit("Too many arguments")
+    elif len(sys.argv) <2 :
+        sys.exit("Too few arguments")
+    else :
+        filename = sys.argv[1]
+        validate(filename)
+        print(display_output(filename))
+
+def validate(filename) :
+    if not filename.endswith(".csv") :
+        sys.exit("Not a csv file")
+    if not os.path.exists(filename) :
+        sys.exit("No such file") 
+
+def display_output(filename) :
+    # data = []
+    with open(filename) as csvfile :
+        data = csv.reader(csvfile)
+        return tabulate.tabulate(data, headers= "firstrow", tablefmt="grid")
+
+main()
